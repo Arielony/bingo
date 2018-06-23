40d4fd88cfea55f42194227646fe491173ba9102
Arieru <ariel_saher@hotmail.com>
2018-06-23 11:41:22 -0400

    Final commit

---
+++ js/servicesJS.js
@@ -0,0 +1,9 @@
+﻿/* Libreria Variables Configuración Servicios URL */
+
+// Login Service
+var service_azure_login =  "http://bingosvcsesion.cloudapp.net/Servicios.svc/api/Login";
+
+// Register Service
+var service_azure_register =  "http://bingosvcsesion.cloudapp.net/Servicios.svc/api/Register";
+
+var service_stats = "http://dev-epsilon.cl/api/Eventos_Usuarios";
